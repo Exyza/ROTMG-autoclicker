febb71287ab4c000cd81e5edb4e36c3a9a40f3de
Eyxza <reidws2021@gmail.com>
2023-03-15 09:17:06 -0700

    Added user friendliness

--- src/clicker.py
+++ src/clicker.py
@@ -1,6 +1,10 @@
 import pyautogui
 import keyboard
 
+print("Hello! Welcome to the ROTMG autoclicker!")
+print("Please place your cursor over the discord channel you wish to rapidly join.")
+print("""When ready, hit 'x' to begin autoclicking. Hit 'c' to stop. This will close the clicker.""")
+
 def clicker():
     while True:
         pyautogui.PAUSE = 0.1
